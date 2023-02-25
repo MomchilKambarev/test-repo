d10b069de0b74fa5c9bab82bba4514b2aad0dbc5
MomchilKambarev <106344525+MomchilKambarev@users.noreply.github.com>
2023-02-25 20:48:13 +0200

    newNew

---
+++ MavenProject/src/test/java/Exercise/Test1.java
@@ -0,0 +1,28 @@
+package Exercise;
+
+import org.testng.annotations.*;
+
+public class Test1 {
+    @BeforeTest
+    public void login() {
+        System.out.println("Before all test methods");
+    }
+
+    @Test
+    public void testMethod1() {
+        System.out.println("Test method1");
+    }
+
+    @Test
+    public void testMethod2() {
+        System.out.println("Test method2");
+    }
+
+    @AfterTest
+    public void logout() {
+        System.out.println("After all the test methods");
+    }
+
+}
+
+
--- src/Lecture6/AverageOfNumbers.java
+++ src/Lecture6/AverageOfNumbers.java
@@ -1,6 +1,6 @@
 package Lecture6;
 
-public class AverageOfNumbers {
+public class AverageOfTreeNumbers {
     public static void main(String[] args) {
         int num1 = 12;
         int num2 = 13;
